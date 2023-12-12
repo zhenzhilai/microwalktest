c3230c356c7817ca79366a9651c563b0f70482b9
zhenzhilai <zhenzhil@student.unimelb.edu.au>
2023-12-12 21:04:30 +1100

    add leak

--- src/my_lib.c
+++ src/my_lib.c
@@ -20,18 +20,40 @@ void lookup_leakage(uint8_t *input, int inputLength, uint8_t *output)
 	// Empty and constant time
 }
 
+
+
+static int NOINLINE branch_leakage_util(uint8_t data)
+{
+    // Use primes and random calculation to keep the compiler from optimizing this function too much
+    
+    if(data < 79)
+        return 7;
+    
+    if(data < 119)
+        return data;
+        
+    return 19 * data + 23;
+}
+
+
 int branch_leakage(uint8_t *input, int inputLength)
 {
 	// Empty and constant time
     
     // add leakages
-//    no leakage reported by follows
     int result = 0;
-
-    for(int i = 0; i < inputLength; ++i){
-        if (input[i] <= 80)
-            break;
-    }
+        
+        for(int i = 0; i < inputLength; ++i)
+            result += branch_leakage_util(input[i]);
+    
+    
+//    no leakage reported by follows
+//    int result = 0;
+//
+//    for(int i = 0; i < inputLength; ++i){
+//        if (input[i] <= 80)
+//            break;
+//    }
 	return lookup[0] + inputLength;
 }
 
